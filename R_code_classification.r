e4333ddf852d428e5461101791792bc8a5447243
benedettabb <80822451+benedettabb@users.noreply.github.com>
2021-04-22 19:34:45 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,5 +1,5 @@
 library(raster)
-
+library(RStoolbox)
 
 setwd ("C:/lab/sun")
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
@@ -10,3 +10,18 @@ so
 #facciamo un plot RGB
 plotRGB (so,1,2,3, stretch = "lin")
 
+#distinguiamo nell'immagine tre livelli energetici, tre classi, scelte da noi (potrebbero essere molte di più).
+#vogliamo fare una classificazione non supervisionata
+#utilizziamo la classificazione unsuperClass (img, numero di classi, 
+so_c <- unsuperClass (so, nClasses = 3)
+
+#dentro so_c ci sono molte cose, il modello con i punti usati, la parte della mappa ecc. 
+#nel plottaggio quello che io voglio plottare è solo la mappa
+#devo legare so_c alla mappa
+plot(so_c$map)
+#il training set è stato selezionato automaticamente (1000pixel) -tra pc e pc il risultato potrebbe essere diverso.
+#per farei in modo che la class sia sempre uguale nel nostro pc usiamo una funzione che si chiama set.seed
+set.seed(42)
+
+#proviamo con un numero di classi molto alto
+so_c20 <- unsuperClass (so, nClasses = 20)
