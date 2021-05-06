5a2543b47e79c3d6fc2288b250773c199d6370bc
benedettabb <80822451+benedettabb@users.noreply.github.com>
2021-05-06 13:04:36 +0200

    Update R_code_landcover.r

--- R_code_landcover.r
+++ R_code_landcover.r
@@ -2,7 +2,18 @@ require(raster)
 require(rasterdiv)
 require(RStoolbox)
 require(ggplo2)
+library(gridExtra)
 
 setwd ("C:/lab/defor")
 defor1 <- brick ("defor1.png")
 defor2 <- brick ("defor2.png")
+
+plotRGB(defor1, r=1, g=2, b=3, stretch ="lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch ="lin")
+
+#se ho installati e caricati il pacchetto ggplot2 e RStoolbox posso fare anche un altro tipo di plot, più bello da vedere.
+p1 <- ggRGB(defor1, r=1, g=2, b=3, stretch ="lin")
+p2 <- ggRGB(defor2, r=1, g=2, b=3, stretch ="lin")
+
+#multiframe con ggplot e gridExtra (plot1, plot2, numero righe)
+grid.arrange(p1, p2, nrow= 2)
